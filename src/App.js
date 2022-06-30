471bfd58a3de997c2947bfd5506e72441d85227e
AnjelinaGit <101161259+AnjelinaGit@users.noreply.github.com>
2022-06-30 15:51:58 +1000

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"> Weather App</div>;
+  return (
+    <div className="App">
+      {" "}
+      Weather App
+      <footer>
+        This project was coded by Anjelina Kabul and is
+        <a
+          href="https://github.com/AnjelinaGit/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
